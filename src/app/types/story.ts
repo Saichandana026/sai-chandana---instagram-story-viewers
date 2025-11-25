f49698312b06267b35224815188fea2bf64c70e7
Sai chandana <saichandana026@gmail.com>
2025-11-25 16:45:43 +0530

    Rename profilePic to avatarUrl in UserStory

--- src/app/types/story.ts
+++ src/app/types/story.ts
@@ -1,14 +1,15 @@
 export interface StoryContent {
   id: string;
   type: "image" | "video";   // defines the type of media
-  mediaUrl: string;          // path to image/video stored inside public folder
+  mediaUrl: string;     
+  timestamp?: string;       // path to image/video stored inside public folder
   viewedBy?: string[];       // store userIds of who viewed this story
 }
 
 export interface UserStory {
   userId: string;
   username: string;
-  profilePic: string;
+  avatarUrl: string; 
   stories: StoryContent[];
 }
 export type Story = {
